54570a5c8e3bdc2053fc586d3e51776db4396a72
Alex <a.sandovalbr@gmail.com>
2020-03-02 21:28:16 -0600

    Project started today, 02-03-2020

---
+++ app/src/main/java/com/example/versus/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.versus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        quantity_picker.minValue = 3
+        quantity_picker.maxValue = 16
+    }
+}
